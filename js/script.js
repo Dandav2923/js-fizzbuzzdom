1752c1f0b8fcfdb71109879bea71031938ca06e3
Dandav2923 <daniele.emili23@gmail.com>
2021-11-23 17:55:27 +0100

    push finale?

--- js/script.js
+++ js/script.js
@@ -1,23 +1,3 @@
-// creare numeri da 1 a 100
-// const container = document.querySelector('.container');
-
-// for (let i = 0; i < 30; i++) {
-//   const element = document.createElement('div');
-//   element.classList.add('box');
-
-//   if (i % 2 == 1) { //resto == 1 allora e dispari
-//     //div verdi
-//     element.classList.add('green');
-//     element.innerHTML = 'numero dispari ' + i;
-//   } else {
-//     // div gialli
-//     element.classList.add('yellow');
-//     element.innerHTML = 'numero pari ' + i;
-//   }
-
-//   container.append(element);
-// }
-
 const container = document.querySelector('.container');
 
 for (let counter = 100; counter > 0; counter--) {
