5f9447f0f969bafd08e69f1f0b2467b84fd817e1
Dandav2923 <daniele.emili23@gmail.com>
2021-11-23 17:40:15 +0100

    push per thomas :)

--- js/script.js
+++ js/script.js
@@ -1,17 +1,49 @@
 // creare numeri da 1 a 100
+// const container = document.querySelector('.container');
+
+// for (let i = 0; i < 30; i++) {
+//   const element = document.createElement('div');
+//   element.classList.add('box');
+
+//   if (i % 2 == 1) { //resto == 1 allora e dispari
+//     //div verdi
+//     element.classList.add('green');
+//     element.innerHTML = 'numero dispari ' + i;
+//   } else {
+//     // div gialli
+//     element.classList.add('yellow');
+//     element.innerHTML = 'numero pari ' + i;
+//   }
+
+//   container.append(element);
+// }
+
 const container = document.querySelector('.container');
 
-for (let i = 0; i < 30; i++) {
-  const element = document.createElement('div');
-  element.classList.add('box');
+for (let counter = 100; counter > 0; counter--) {
+    // console.log(counter);
+    const element = document.createElement('div');
+    element.classList.add('box');
 
-  if (i % 2 == 1) { //resto == 1 allora e dispari
-    //div verdi
-    element.classList.add('green');
-    element.innerHTML = 'numero dispari ' + i;
-  } else {
-    // div gialli
-    element.classList.add('yellow');
-    element.innerHTML = 'numero pari ' + i;
-  }
+    if (counter % 3 == 0 && counter % 5 == 0) {
+        // console.log('fizz');
+        element.classList.add('green');
+        element.innerHTML = 'fizzbuzz';
+    }
+    else if (counter % 3 == 0) {
+        element.classList.add('red');
+        element.innerHTML = 'fizz';
+    }
+    else if (counter % 5 ==0) {
+        // console.log('buzz');
+        element.classList.add('yellow');
+        element.innerHTML = 'buzz';
+    } 
+    else {
+        // console.log(counter);
+        element.innerHTML = counter;
+        element.classList.add('blu');
+    }
+
+    container.append(element);
 }
