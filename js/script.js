fc4c8f2766b6f92595a17663d32c81c53083b880
Dandav2923 <daniele.emili23@gmail.com>
2022-01-15 16:15:21 +0100

    fix

--- js/script.js
+++ js/script.js
@@ -1,22 +1,22 @@
 const container = document.querySelector('.container');
 
 for (let counter = 100; counter > 0; counter--) {
-    console.log(counter);
+    // console.log(counter);
     const element = document.createElement('div');
     element.classList.add('box');
 
     if (counter % 3 == 0 && counter % 5 == 0) {
-        console.log('fizzbuzz');
+        // console.log('fizzbuzz');
         element.classList.add('green');
         element.innerHTML = 'fizzbuzz';
     }
     else if (counter % 3 == 0) {
-        console.log('fizz');
+        // console.log('fizz');
         element.classList.add('red');
         element.innerHTML = 'fizz';
     }
     else if (counter % 5 ==0) {
-        console.log('buzz');
+        // console.log('buzz');
         element.classList.add('yellow');
         element.innerHTML = 'buzz';
     } 
